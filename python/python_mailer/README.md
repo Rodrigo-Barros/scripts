4850d0d9f0c4e5981af327b1f6da60847f41ab27
Rodrigo Gabriel <rodrigogenio12@gmail.com>
2019-10-01 15:35:04 -0300

    adicionado o script de envio de emails

---
+++ python/python_mailer/README.md
@@ -0,0 +1,3 @@
+para usar esse script basta informar os destinatarios no arquivo de texto
+destinatarios.txt sempre pulando uma linha para cada destinatarios e então
+o script funcionará
---
+++ python/python_mailer/send_emails_bomber.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+import smtplib
+from getpass import getpass
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+#anexo
+from email import encoders
+from email.mime.base import MIMEBase
+import os
+import mimetypes
+
+#email = "rodrigogenio12@gmail.com"
+email = "rodrigogcbarros@gmail.com"
+secure_port = 587
+destinatarios=open('destinatarios.txt','r')
+token='ycbiddwmtlmfgmfg'
+nome_do_anexo='Rodrigo_Gabriel_Clemente_de_Barros_Currículo.pdf'
+
+content_message='Olá me chamo Rodrigo e estou procurando uma oportunidade de trabalho, por favor olhe as minhas atribuições no currículo anexo a esta mensagem.'
+print('email: %s' % email)
+try:
+    for destinatario in destinatarios:
+        msg = MIMEMultipart()
+        msg["Subject"] = "Currículo | Rodrigo Barros"
+        msg["From"] = "Rodrigo Barros <rodrigogcbarros@gmail.com>"
+        msg["To"] = destinatario
+        msg.attach(MIMEText(content_message, 'plain','utf-8'))
+
+        #anexo
+        anexo='/home/rodrigo/Desktop/Rodrigo_Gabriel_Clemente_de_Barros_Currículo.pdf'
+        ctype, encoding = mimetypes.guess_type(anexo)
+
+        maintype, subtype = ctype.split('/', 1)
+        mime = MIMEBase(maintype, subtype)
+        f=open(anexo,'rb')
+        mime.set_payload(f.read())
+        mime = MIMEBase(maintype, subtype)
+        encoders.encode_base64(mime)
+        mime.add_header('Content-Disposition', 'attachment', filename=nome_do_anexo)
+        msg.attach(mime)
+
+        #adiciona_anexo(msg,'/home/rodrigo/Desktop/Rodrigo_Gabriel_Clemente_de_Barros_Currículo.pdf')
+
+        email_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        email_server.login(email, token)
+        email_server.sendmail(email, destinatario, msg.as_string())
+        email_server.close()
+        print("Email enviado com sucesso para o destinario %s" % destinatario)
+except KeyboardInterrupt:
+    print("\nabortando...")
+except Exception as e:
+    print(e)
